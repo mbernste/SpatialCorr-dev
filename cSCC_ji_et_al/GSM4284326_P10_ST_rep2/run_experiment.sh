44b5662aace63dd0629de1a7a35bd1e8a8ead284
Matthew Bernstein <matthewbernstein@Matthews-MacBook-Pro-2.local>
2021-10-22 13:47:15 -0500

    Run on P10 Ji et al.

---
+++ cSCC_ji_et_al/GSM4284326_P10_ST_rep2/run_experiment.sh
@@ -0,0 +1,41 @@
+# WHR-Test
+python run_whr_test.py -c -n 11 -g gene_set_groups/keratinocyte_related.tsv -o results/GO_KERATINOCYTE_DIFFERENTIATION.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/cell_cell_adhesion_related.tsv -o results/GO_CELL_CELL_ADHESION.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/apoptosis_related.tsv -o results/APOPTOSIS.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/epithelial_related.tsv -o results/EPITHELIAL.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/b_cell_related.tsv -o results/B_CELL.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/t_cell_related.tsv -o results/T_CELL.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/hypoxia_related.tsv -o results/HYPOXIA.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/cell_division_related.tsv -o results/CELL_DIVISION.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/migration_related.tsv -o results/MIGRATION.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/dna_repair_related.tsv -o results/DNA_REPAIR.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/cell_death_related.tsv -o results/CELL_DEATH.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/dna_replication_related.tsv -o results/DNA_REPLICATION.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/matrix_related.tsv -o results/MATRIX.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/calcium_related.tsv -o results/CALCIUM.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/interferon_related.tsv -o results/INTERFERON.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/interleukin_related.tsv -o results/INTERLEUKIN_RELATED.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/angiogenesis_related.tsv -o results/ANGIOGENESIS_RELATED.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/epidermis_development_related.tsv -o results/EPIDERMIS_DEVELOPMENT.WHR.json
+python run_whr_test.py -c -n 11 -g gene_set_groups/fibroblast_related.tsv -o results/FIBROBLAST.WHR.json
+
+# BHR-Test
+python run_bhr_test.py -c -n 11 -g gene_set_groups/keratinocyte_related.tsv -o results/GO_KERATINOCYTE_DIFFERENTIATION.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/cell_cell_adhesion_related.tsv -o results/GO_CELL_CELL_ADHESION.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/apoptosis_related.tsv -o results/APOPTOSIS.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/epithelial_related.tsv -o results/EPITHELIAL.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/b_cell_related.tsv -o results/B_CELL.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/t_cell_related.tsv -o results/T_CELL.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/hypoxia_related.tsv -o results/HYPOXIA.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/cell_division_related.tsv -o results/CELL_DIVISION.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/migration_related.tsv -o results/MIGRATION.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/dna_repair_related.tsv  -o results/DNA_REPAIR.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/cell_death_related.tsv -o results/CELL_DEATH.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/dna_replication_related.tsv -o results/DNA_REPLICATION.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/matrix_related.tsv -o results/MATRIX.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/calcium_related.tsv -o results/CALCIUM.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/interferon_related.tsv -o results/INTERFERON.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/interleukin_related.tsv -o results/INTERLEUKIN_RELATED.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/angiogenesis_related.tsv -o results/ANGIOGENESIS_RELATED.BHR.json
+python run_bhr_test.py -c -n 11 -g gene_set_groups/epidermis_development_related.tsv -o results/EPIDERMIS_DEVELOPMENT.BHR.json
+
